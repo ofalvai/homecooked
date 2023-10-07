ffa1136ef4865f3faccb9644f45936bdb0f15f78
Olivér Falvai <ofalvai@gmail.com>
2023-10-07 14:18:30 +0200

    Avoid static rendering

--- llm-ui/app/api/config/route.ts
+++ llm-ui/app/api/config/route.ts
@@ -1,5 +1,8 @@
 import { AppConfig } from "@/lib/types"
 
+// Force Next.js to treat this as a dynamic route because of reading runtime-defined env vars
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const baseUrl = process.env.LLM_API_BASE_URL
   if (!baseUrl) throw new Error("Missing env var LLM_API_BASE_URL")
